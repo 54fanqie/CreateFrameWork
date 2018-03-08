bacf97b4bfd31c487c5c892d6bc54a6aa7349295
54fanqie <2917545349@qq.com>
2018-03-08 20:28:49 +0800

    Initial Commit

---
+++ CreateFrameWork/CreateFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  CreateFrameWork.h
+//  CreateFrameWork
+//
+//  Created by 番茄 on 3/8/18.
+//  Copyright © 2018 fanqie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CreateFrameWork.
+FOUNDATION_EXPORT double CreateFrameWorkVersionNumber;
+
+//! Project version string for CreateFrameWork.
+FOUNDATION_EXPORT const unsigned char CreateFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CreateFrameWork/PublicHeader.h>
+
+
